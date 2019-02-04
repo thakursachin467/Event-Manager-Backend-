9358adca11159327540b55e78a1ba83359be0f58
Sachin Thakur <thakursachin467@gmail.com>
2019-02-04 23:45:23 +0530

    database added

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const graphqlhttp= require('express-graphql');
 const {buildSchema}= require('graphql');
-
+const mongoose = require('mongoose');
 const app = express();
 const port=5001;
 // view engine setup
-
+const events=[];
 
 app.use('/graphql',graphqlhttp({
   schema:buildSchema(`
+  type Event{
+  _id:ID!
+  title:String!
+  description:String!
+  price:Float!
+  date:String!
+  }
+  
+  input EventInput{
+  title:String!
+  description:String!
+  price:Float!
+  date:String!
+  
+  } 
     type rootQuery{
-        events: [String!]!,
+        events: [Event!]!,
         newEvents:String!
     }
     
     type rootMutation{
-        createEvent(name:String): String
+        createEvent(eventInput:EventInput): Event
     }
     schema {
       query:rootQuery
@@ -26,11 +41,18 @@ app.use('/graphql',graphqlhttp({
   `),
   rootValue:{
     events:()=> {
-      return ['sachin', 'thakur'];
+      return events;
     },
     createEvent:(args)=>{
-      const Eventname=args.name;
-      return Eventname;
+      const event={
+        _id:Math.random().toString(),
+        title:args.eventInput.title,
+        description:args.eventInput.description,
+        price: +args.eventInput.price,
+        date:new Date().toISOString()
+      };
+      events.push(event);
+      return event;
     },
     newEvents:(args)=>{
       return 'You entered a name of new event';
@@ -39,6 +61,9 @@ app.use('/graphql',graphqlhttp({
   graphiql:true
 }));
 
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-itdbo.mongodb.net/test?retryWrites=true`)
+    .then(()=>console.log(`connection to database success`))
+    .catch(((err)=>console.log(`error ${err}`)));
 app.listen(port,()=>{
   console.log(`server started on port ${port}`);
 });
